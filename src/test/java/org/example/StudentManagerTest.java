d2710d823de064b85b238e431332aaedc98498a8
Anika Tahsin <anna.tahsin20@gmail.com>
2025-07-24 01:27:22 +0600

    Added Junit lifecycle annotations

--- src/test/java/org/example/StudentManagerTest.java
+++ src/test/java/org/example/StudentManagerTest.java
@@ -1,6 +1,28 @@
 package org.example;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 class StudentManagerTest {
-  
+    private  StudentManager manager;
+    @BeforeEach
+    void setupAll(){
+        System.out.println("String Student manager tests");
+    }
+    @BeforeEach
+    void setup(){
+        manager=new StudentManager("Alice",80,90,86);
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("Test finished");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("All tests done.");
+    }
 }
